be35302a03803d87018b7fb40d4a8844164fe2c9
Gianfranco Palumbo <gianpa@gmail.com>
2017-12-18 13:30:14 +0200

    add devtools redux for React Native Debugger app

--- src/store.js
+++ src/store.js
@@ -31,7 +31,12 @@ const rootReducer = combineReducers({
 });
 
 function configureStore() {
-  const store = createStore(rootReducer, applyMiddleware(thunk));
+  const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(thunk)
+  );
   const persistor = persistStore(store);
   // persistor.purge();
   return { persistor, store };

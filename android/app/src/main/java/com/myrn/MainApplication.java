f4a3f5c8db197413c82e78f2f3c5b71aaf2955e5
Gianfranco Palumbo <gianpa@gmail.com>
2017-12-23 16:34:55 +0200

    try detox android testing

--- android/app/src/main/java/com/myrn/MainApplication.java
+++ android/app/src/main/java/com/myrn/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.reactnative.ivpusic.imagepicker.PickerPackage;
-import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.dylanvann.fastimage.FastImageViewPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -28,7 +27,6 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new PickerPackage(),
-            new RNGestureHandlerPackage(),
             new FastImageViewPackage(),
             new VectorIconsPackage()
       );

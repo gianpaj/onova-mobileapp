91a53b39d01cf330e38756a88a9dff2e65700730
Gianfranco Palumbo <gianpa@gmail.com>
2018-01-22 19:20:27 +0200

    refactor: remove counter reducer

--- src/store.js
+++ src/store.js
@@ -3,29 +3,20 @@ import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/es/storage';
 import thunk from 'redux-thunk';
 
-import counterReducer from './reducers/counterReducer';
 import NavigationReducer from './reducers/navigationReducer';
 import loginReducer from './reducers/loginReducer';
-
-// config to not persist the *counterString* of the CounterReducer's slice of the global state.
-const config = {
-  key: 'root',
-  storage,
-  blacklist: ['counterString'],
-};
 
 const config1 = {
   key: 'primary',
   storage,
+  // blacklist: ['counterString'],
 };
 
 // We are only persisting the counterReducer and loginReducer
-const CounterReducer = persistReducer(config, counterReducer);
 const LoginReducer = persistReducer(config1, loginReducer);
 
 // combineReducer applied on persisted(counterReducer) and NavigationReducer
 const rootReducer = combineReducers({
-  CounterReducer,
   NavigationReducer,
   LoginReducer,
 });

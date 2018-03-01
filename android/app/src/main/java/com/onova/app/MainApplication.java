ec3644fd5c51dff8e58f2f8c11cd5e72928cfb8e
Gianfranco Palumbo <gianpa@gmail.com>
2018-03-01 12:03:02 +0000

    Rename app to Onova

---
+++ android/app/src/main/java/com/onova/app/MainApplication.java
@@ -0,0 +1,59 @@
+package com.onova.app;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.lottie.LottiePackage;
+import com.pw.droplet.braintree.BraintreePackage;
+import com.RNFetchBlob.RNFetchBlobPackage;
+import com.reactnative.ivpusic.imagepicker.PickerPackage;
+import com.dylanvann.fastimage.FastImageViewPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+            new LottiePackage(),
+            new BraintreePackage(),
+            new RNFetchBlobPackage(),
+            new PickerPackage(),
+            new FastImageViewPackage(),
+            new VectorIconsPackage(),
+            new ReactNativePushNotificationPackage()
+      );
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
+}
---
+++ ios/Pods/Target Support Files/Pods-onova-tvOSTests/Pods-onova-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_onova_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_onova_tvOSTests
+@end

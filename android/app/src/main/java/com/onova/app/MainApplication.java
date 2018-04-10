8dd193c14c30a00805b5b24b4da0aa494bd9eed4
Gianfranco Palumbo <gianpa@gmail.com>
2018-04-10 13:35:38 +0300

    android: install firebase messaging and notifications

--- android/app/src/main/java/com/onova/app/MainApplication.java
+++ android/app/src/main/java/com/onova/app/MainApplication.java
@@ -8,6 +8,8 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
+import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
 import com.reactnative.ivpusic.imagepicker.PickerPackage;
 
 import java.util.Arrays;
@@ -26,6 +28,8 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new RNFirebasePackage(),
+            new RNFirebaseMessagingPackage(),
+            new RNFirebaseNotificationsPackage(),
             new PickerPackage()
       );
     }

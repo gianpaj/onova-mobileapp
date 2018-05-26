1485f62799c3f8ba5fa8f03bfec07ed32cd271a0
Gianfranco Palumbo <gianpa@gmail.com>
2018-05-26 13:58:08 +0300

    add workarounds for RN 0.55.x

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 // @flow
 
-import { AppRegistry } from 'react-native';
+import { AppRegistry, YellowBox } from 'react-native';
 import App from './src/App';
+
+YellowBox.ignoreWarnings([
+  // workaround for https://github.com/facebook/react-native/issues/18868
+  'Warning: isMounted(...) is deprecated',
+  // workaround until https://github.com/joltup/react-native-fetch-blob is published to npm
+  'Module RNFetchBlob requires main',
+  // workaround for https://github.com/facebook/react-native/issues/17504
+  // until is 0.56.0 is released
+  'Module RCTImageLoader requires main',
+]);
 
 AppRegistry.registerComponent('onova', () => App);

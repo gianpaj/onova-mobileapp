409d4e1ed78186ef23fad9ca5127ab65e1391979
Gianfranco Palumbo <gianpa@gmail.com>
2018-06-28 11:38:30 +0100

    comments on RCTCxxModule fix

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ YellowBox.ignoreWarnings([
   // workaround for https://github.com/facebook/react-native/issues/17504
   // until is 0.56.0 is released
   'Module RCTImageLoader requires main',
-  // bug...
+  // bug https://github.com/facebook/react-native/pull/19880
+  // should be fixed in 0.56 https://github.com/react-native-community/react-native-releases/issues/14#issuecomment-400954904
   'Class RCTCxxModule was not exported',
 ]);
 

53e1ec716250ac4a41fbbf28555a655b8d12ce6e
Gianfranco Palumbo <gianpa@gmail.com>
2018-07-05 14:33:33 +0100

    fix enzyme tests

--- __tests__/components/HR.test.js
+++ __tests__/components/HR.test.js
@@ -3,8 +3,13 @@ import renderer from 'react-test-renderer';
 import HR from '../../src/components/HR';
 
 describe('HR', () => {
-  it('renders correctly', () => {
+  it('renders correctly normal width', () => {
     const tree = renderer.create(<HR />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders correctly full width', () => {
+    const tree = renderer.create(<HR full />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 });

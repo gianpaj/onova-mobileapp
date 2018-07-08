39a135b60f67e032f72719ef4db994967184bb49
Gianfranco Palumbo <gianpa@gmail.com>
2018-07-08 18:46:52 +0300

    fix tests

--- __tests__/components/Header.test.js
+++ __tests__/components/Header.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-// import Header from '../../src/components/Header';
+import Header from '../../src/components/Header';
 
-describe.skip('Header', () => {
+describe('Header', () => {
   it('renders correctly', () => {
     const tree = renderer.create(<Header />).toJSON();
     expect(tree).toMatchSnapshot();

42d79a3da42c5a25006945e0e64906eff9518a82
Gianfranco Palumbo <gianpa@gmail.com>
2018-11-27 21:40:33 +0200

    Add breadcrumbs for locale

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,15 +1,25 @@
+// @flow
+
 import I18n from 'react-native-i18n';
+import { Sentry } from 'react-native-sentry';
+
 import en from './locales/en';
 import uk from './locales/uk';
 
 I18n.fallbacks = true;
 
-const a = I18n.currentLocale();
+const locale = I18n.currentLocale();
 
-if (a.startsWith('ru')) {
+if (locale.startsWith('ru')) {
   I18n.locale = 'uk-UA';
   console.debug('Russian language detected. Fallbacking to Ukrainian');
 }
+
+Sentry.captureBreadcrumb({
+  category: 'user-settings',
+  message: locale,
+  level: 'info',
+});
 
 I18n.translations = {
   en,

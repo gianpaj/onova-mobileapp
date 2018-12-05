78c2b6480eb7483e87f8f12c948410dde73c492c
Gianfranco Palumbo <gianpa@gmail.com>
2018-12-05 12:59:44 +0200

    fix builds

--- android/app/src/main/java/com/onova/app/MainApplication.java
+++ android/app/src/main/java/com/onova/app/MainApplication.java
@@ -3,22 +3,19 @@ package com.onova.app;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.heanoria.library.reactnative.locationenabler.RNAndroidLocationEnablerPackage;
+import com.leo_pharma.analytics.AnalyticsPackage;
 import com.horcrux.svg.SvgPackage;
 import com.reactnativecommunity.webview.RNCWebViewPackage;
+import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
+import com.reactnative.ivpusic.imagepicker.PickerPackage;
+import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
+import io.sentry.RNSentryPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
-import com.heanoria.library.reactnative.locationenabler.RNAndroidLocationEnablerPackage;
-import com.leo_pharma.analytics.AnalyticsPackage;
-import com.reactnative.ivpusic.imagepicker.PickerPackage;
-import com.RNFetchBlob.RNFetchBlobPackage;
-import com.segment.analytics.android.integrations.mixpanel.MixpanelIntegration;
-import io.invertase.firebase.RNFirebasePackage;
-import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
-import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
-import io.sentry.RNSentryPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -35,17 +32,15 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNAndroidLocationEnablerPackage(),
+            new AnalyticsPackage(),
             new SvgPackage(),
             new RNCWebViewPackage(),
-            new RNAndroidLocationEnablerPackage(),
-            new RNFetchBlobPackage(),
+            new RNI18nPackage(),
             new PickerPackage(),
             new RNFirebasePackage(),
             new RNFirebaseMessagingPackage(),
-            new RNFirebaseNotificationsPackage(),
-            new RNI18nPackage(),
-            new RNSentryPackage(),
-            new AnalyticsPackage()
+            new RNSentryPackage()
       );
     }
 

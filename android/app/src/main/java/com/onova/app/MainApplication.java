c24d30e8cbecf1a08dc7d059e8c9e1c48b529679
Gianfranco Palumbo <gianpa@gmail.com>
2019-03-28 12:44:23 +0200

    Android: add missing dependencies for react-native-reanimated

--- android/app/src/main/java/com/onova/app/MainApplication.java
+++ android/app/src/main/java/com/onova/app/MainApplication.java
@@ -3,6 +3,7 @@ package com.onova.app;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.swmansion.reanimated.ReanimatedPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.heanoria.library.reactnative.locationenabler.RNAndroidLocationEnablerPackage;
 import com.leo_pharma.analytics.AnalyticsPackage;
@@ -34,6 +35,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReanimatedPackage(),
             new RNGestureHandlerPackage(),
             new RNAndroidLocationEnablerPackage(),
             new AnalyticsPackage(),

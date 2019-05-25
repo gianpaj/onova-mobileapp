d2542750ff55f97474390f723e26752711c1858c
Gianfranco Palumbo <gianpa@gmail.com>
2019-05-25 17:54:05 +0300

    ignore 'Require cycle' warnings

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
  * @format
  */
 
-import { AppRegistry } from 'react-native';
+import { AppRegistry, YellowBox } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
+
+// ignore specific yellowbox warnings
+YellowBox.ignoreWarnings(['Require cycle:']);
 
 AppRegistry.registerComponent(appName, () => App);

a52b49ae7483a51d439316d202828b9ca684305e
Gianfranco Palumbo <gianpa@gmail.com>
2019-05-26 18:50:49 +0300

    refactor(IntroScreens): use functional component

--- src/screens/IntroScreens.js
+++ src/screens/IntroScreens.js
@@ -1,6 +1,6 @@
 // @flow
 
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import Onboarding from 'react-native-onboarding-swiper';
 import Icon from 'react-native-vector-icons/Ionicons';
@@ -17,43 +17,45 @@ type Props = {
   dispatch: Dispatch,
 };
 
-class IntroScreens extends Component<Props> {
-  onFinish = () => this.props.dispatch(logout());
-
-  render() {
-    return (
-      <Onboarding
-        skipLabel={I18n.t('intro.skip')}
-        nextLabel={I18n.t('intro.next')}
-        onSkip={this.onFinish}
-        imageContainerStyles={{ marginTop: -36, paddingBottom: 36 }}
-        bottomBarHighlight={false}
-        DoneButtonComponent={() => (
-          <MaterialIcons color={colors.black} name="check" size={36} style={{ left: -10 }} onPress={this.onFinish} />
-        )}
-        pages={[
-          {
-            backgroundColor: colors.white,
-            image: <Shield width={80} height={80} />,
-            title: I18n.t('intro.step_1.title'),
-            subtitle: I18n.t('intro.step_1.subtitle'),
-          },
-          {
-            backgroundColor: colors.white,
-            image: <ShoeLabel width={80} height={80} />,
-            title: I18n.t('intro.step_2.title'),
-            subtitle: I18n.t('intro.step_2.subtitle'),
-          },
-          {
-            backgroundColor: colors.white,
-            image: <Icon color={colors.black} name="md-person-add" size={80} />,
-            title: I18n.t('intro.step_3.title'),
-            subtitle: I18n.t('intro.step_3.subtitle'),
-          },
-        ]}
-      />
-    );
-  }
+function IntroScreens(props: Props) {
+  return (
+    <Onboarding
+      skipLabel={I18n.t('intro.skip')}
+      nextLabel={I18n.t('intro.next')}
+      onSkip={() => props.dispatch(logout())}
+      imageContainerStyles={{ marginTop: -36, paddingBottom: 36 }}
+      bottomBarHighlight={false}
+      DoneButtonComponent={() => (
+        <MaterialIcons
+          color={colors.black}
+          name="check"
+          size={36}
+          style={{ left: -10 }}
+          onPress={() => props.dispatch(logout())}
+        />
+      )}
+      pages={[
+        {
+          backgroundColor: colors.white,
+          image: <Shield width={80} height={80} />,
+          title: I18n.t('intro.step_1.title'),
+          subtitle: I18n.t('intro.step_1.subtitle'),
+        },
+        {
+          backgroundColor: colors.white,
+          image: <ShoeLabel width={80} height={80} />,
+          title: I18n.t('intro.step_2.title'),
+          subtitle: I18n.t('intro.step_2.subtitle'),
+        },
+        {
+          backgroundColor: colors.white,
+          image: <Icon color={colors.black} name="md-person-add" size={80} />,
+          title: I18n.t('intro.step_3.title'),
+          subtitle: I18n.t('intro.step_3.subtitle'),
+        },
+      ]}
+    />
+  );
 }
 
 export default connect()(IntroScreens);

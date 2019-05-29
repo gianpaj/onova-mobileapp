eacb8606c71033445863e820fb398341e4a0b9b8
Gianfranco Palumbo <gianpa@gmail.com>
2019-05-29 10:31:06 +0300

    [ios] Locale setting for react-native-modal-datetime-picker

--- ios/onova/AppDelegate.m
+++ ios/onova/AppDelegate.m
@@ -34,6 +34,11 @@
 
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
+  // for react-native-modal-datetime-picker
+  // Force DatePicker locale to current language (for: 24h or 12h format, full day names, .etc...)
+  NSString *currentLanguage = [[NSLocale preferredLanguages] firstObject];
+  [[UIDatePicker appearance] setLocale:[[NSLocale alloc]initWithLocaleIdentifier:currentLanguage]];
+
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
